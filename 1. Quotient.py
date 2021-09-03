e9dc99470dcc8f6963dce2a25e12509a9dbe41ce
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-02 22:41:49 -0700

    Create 1. Quotient.py

---
+++ 1. Quotient.py
@@ -0,0 +1,18 @@
+# Huu Hung Nguyen
+# September 2, 2021
+# Quotient.py
+# The program will prompt the user for a numerator and a denominator.
+# The program performs the division and displays the output.
+
+
+# Prompt the user for the numerator
+numerator = input('Enter a numerator: ')
+
+# Prompt the user for the denominator
+denominator = input('Enter a denominator: ')
+
+# Perform the division 
+quotient = int(numerator) / int(denominator)
+
+# Display the quotient
+print('Quotient: ', quotient, '.', sep = '')
