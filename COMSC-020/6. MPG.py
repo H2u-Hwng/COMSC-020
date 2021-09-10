464d47414d611e964142a0b45901b12a21b6f240
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-09 23:16:25 -0700

    Create 6. MPG.py

---
+++ COMSC-020/6. MPG.py
@@ -0,0 +1,18 @@
+# Huu Hung Nguyen
+# 09/09/2021
+# MPG.py
+# The program prompts the user for the miles driven and gallons used.
+# It then computes miles per gallon and prints the result.
+
+
+# Prompt user for miles
+miles = float(input('How many miles driven? '))
+
+# Prompt user for gallons
+gallons = float(input('How many gallons used? '))
+
+# Compute miles per gallon
+mpg = miles / gallons
+
+# Print result
+print('The car can travel {:,.3f} miles per gallon.'.format(mpg))
