9681aad7fc40ac08039529c8b17654824bf82659
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-19 16:20:40 -0700

    Add files via upload

--- COMSC-020/Nguyen_HuuHung_windchill.py
+++ COMSC-020/Nguyen_HuuHung_windchill.py
@@ -1,15 +1,15 @@
 # Huu Hung Nguyen
-# 09/13/2021
+# 09/19/2021
 # Nguyen_HuuHung_windchill.py
-# The program prompts the user for the temperature in celsius,
-# and the wind velocity.
+# The program prompts the user for the temperature in Celsius,
+# and the wind velocity in kilometers per hour.
 # It then calculates the wind chill temperature and prints the result.
 
 
-# Prompt user for temperature in celsius
+# Prompt user for temperature in Celsius
 t = float(input('Enter temperature in degrees Celsius: '))
 
-# Prompt user for wind velocity
+# Prompt user for wind velocity in kilometers per hour
 v = float(input('Enter wind velocity in kilometers/hour: '))
 
 # Calculate wind chill temperature
