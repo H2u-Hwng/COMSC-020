65203f351450d313c5ad370360f3b963fe7abf79
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-19 16:49:49 -0700

    Add files via upload

--- COMSC-020/Nguyen_HuuHung_payment.py
+++ COMSC-020/Nguyen_HuuHung_payment.py
@@ -1,9 +1,9 @@
 # Huu Hung Nguyen
-# 09/13/2021
+# 09/19/2021
 # Nguyen_HuuHung_payment.py
 # The program prompts the user for the amount of the loan,
 # the annual percentage rate, and the number of years of the loan.
-# It then calculates monthly payments on a loan and prints the result.
+# It then calculates the monthly payment and prints the result.
 
 
 # Prompt user for principal
@@ -16,13 +16,16 @@ annual_rate = float(input('Enter annual interest rate as a percent: '))
 years = int(input('Enter number of years for the loan: '))
 
 # Calculate monthly interest rate
-r = annual_rate / 12
+r = (annual_rate / 12) / 100 
 
 # Calculate number of months of the loan
 n = years * 12
 
+# Common subformula
+common = (1 + r)**n
+
 # Calculate monthly payment on the loan
-payment = p * r/100 * (1 + r/100)**n / ((1+r/100)**n - 1)
+payment = p * r * common / (common - 1)
 
 # Print result
-print('Your monthly payment is ${:.2f}.'.format(payment))
+print('Your monthly payment is ${:.2f}'. format(payment))
