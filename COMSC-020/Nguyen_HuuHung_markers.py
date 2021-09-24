207ebc262db4b7f43620a96f0860cf45a493b999
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-23 23:41:05 -0700

    Add files via upload

--- COMSC-020/Nguyen_HuuHung_markers.py
+++ COMSC-020/Nguyen_HuuHung_markers.py
@@ -1,30 +1,37 @@
 # Huu Hung Nguyen
-# 09/20/2021
+# 09/23/2021
 # Nguyen_HuuHung_markers.py
-# The program prompts the user for the number of markers purchased.
-# It calculates the number of packages, the number of separate markers,
-# the subtotal, the tax, the shipping cost, and the total cost.
-# It then prints all of them.
+# The program assumes the price of 1 marker is 80 cents, 5 markers is $3.50,
+# the tax sale rate is 6.5%, the shipping rate is 5%.
+# It prompts the user for the number of markers.
+# It calculates and displays the number of packages 5 markers,
+# the number of separate markers, the subtotal, the tax cost,
+# the shipping cost, and the total cost.
 
+# Assume price of 1 marker, 5 markers, tax sale rate and shipping rate
+PRICE1 = 0.80
+PRICE5 = 3.50
+TAX_SALE_RATE = 6.5 / 100
+SHIPPING_RATE = 5 / 100
 
-# Prompt user for number of markers purchased
+# Prompt user for number of markers
 num_markers = int(input('How many markers are you buying? '))
 
-# calculates number of packages and number of separate markers
+# Calculate number of packages 5 markers and separate markers
 num_packages = num_markers // 5
 num_separate_markers = num_markers % 5
 
-# Calculate subtotal, tax, shipping cost, and total cost
-subtotal = num_packages * 3.50 + num_separate_markers * 0.80 
-tax = subtotal * 6.5 / 100
-shipping_cost = subtotal * 5 / 100
-total_cost = subtotal + shipping_cost + tax
+# Calculate subtotal, tax cost, shipping cost, and total cost
+subtotal = num_packages * PRICE5 + num_separate_markers * PRICE1
+tax_cost = subtotal * TAX_SALE_RATE
+shipping_cost = subtotal * SHIPPING_RATE
+total_cost = subtotal + tax_cost + shipping_cost
 
-# Print all of them
+# Display program
 print('''Number of packages: {}
 Number of separate markers: {}
 Subtotal: ${:.2f}
 Tax:      ${:.2f}
 Shipping: ${:.2f}
 Total:    ${:.2f}'''. format(num_packages, num_separate_markers,
-                             subtotal, tax, shipping_cost, total_cost))
+                             subtotal, tax_cost, shipping_cost, total_cost))
