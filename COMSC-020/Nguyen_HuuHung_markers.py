6aa236e08b7c0c095a29162030d1760e52b9e5aa
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-24 23:09:03 -0700

    Add files via upload

--- COMSC-020/Nguyen_HuuHung_markers.py
+++ COMSC-020/Nguyen_HuuHung_markers.py
@@ -1,5 +1,5 @@
 # Huu Hung Nguyen
-# 09/23/2021
+# 09/24/2021
 # Nguyen_HuuHung_markers.py
 # The program assumes the price of 1 marker is 80 cents, 5 markers is $3.50,
 # the tax sale rate is 6.5%, the shipping rate is 5%.
@@ -8,21 +8,24 @@
 # the number of separate markers, the subtotal, the tax cost,
 # the shipping cost, and the total cost.
 
-# Assume price of 1 marker, 5 markers, tax sale rate and shipping rate
-PRICE1 = 0.80
-PRICE5 = 3.50
-TAX_SALE_RATE = 6.5 / 100
-SHIPPING_RATE = 5 / 100
+# Assume price of 1 marker, 5 markers, and number of markers per package
+PRICE_MARKER = 0.80
+PRICE_PACKAGE = 3.50
+NUM_MARKERS_PER_PACKAGE = 5
+
+# Assume tax sale rate and shipping rate
+TAX_SALE_RATE = 0.065
+SHIPPING_RATE = 0.05
 
 # Prompt user for number of markers
 num_markers = int(input('How many markers are you buying? '))
 
 # Calculate number of packages 5 markers and separate markers
-num_packages = num_markers // 5
-num_separate_markers = num_markers % 5
+num_packages = num_markers // NUM_MARKERS_PER_PACKAGE
+num_separate_markers = num_markers % NUM_MARKERS_PER_PACKAGE
 
 # Calculate subtotal, tax cost, shipping cost, and total cost
-subtotal = num_packages * PRICE5 + num_separate_markers * PRICE1
+subtotal = num_packages * PRICE_PACKAGE + num_separate_markers * PRICE_MARKER
 tax_cost = subtotal * TAX_SALE_RATE
 shipping_cost = subtotal * SHIPPING_RATE
 total_cost = subtotal + tax_cost + shipping_cost
