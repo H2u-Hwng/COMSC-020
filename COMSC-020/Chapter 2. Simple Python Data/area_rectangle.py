f1f9df796b640ca4ccd5015e49f1d816c520292e
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-29 21:24:55 -0700

    Update and rename COMSC-020/Nguyen_HuuHung_AreaRectangle.py to COMSC-020/Chapter 2. Simple Python Data/area_rectangle.py

--- COMSC-020/Chapter 2. Simple Python Data/area_rectangle.py
+++ COMSC-020/Chapter 2. Simple Python Data/area_rectangle.py
@@ -1,9 +1,8 @@
 # Huu Hung Nguyen
 # 09/21/2021
-# Nguyen_HuuHung_AreaRectangle.py
+# area_rectangle.py
 # The program prompts the user for the width and height.
 # It then calculates the the area of the rectangle and prints the result.
-
 
 # Prompt user for width and height
 width = float(input('Enter the width of the rectangle: '))
