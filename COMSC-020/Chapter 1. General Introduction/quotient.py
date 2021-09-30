4ba5d116f5af46021dcabc2dab123f47faa532e8
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-09-29 21:50:10 -0700

    Update and rename COMSC-020/1. Quotient.py to COMSC-020/Chapter 1. General Introduction/quotient.py

--- COMSC-020/Chapter 1. General Introduction/quotient.py
+++ COMSC-020/Chapter 1. General Introduction/quotient.py
@@ -1,9 +1,8 @@
 # Huu Hung Nguyen
 # September 2, 2021
-# Quotient.py
+# quotient.py
 # The program will prompt the user for a numerator and a denominator.
 # The program performs the division and displays the output.
-
 
 # Prompt the user for the numerator
 numerator = input('Enter a numerator: ')
