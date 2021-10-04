a4b4a91f84750a7b12796da9a0080ec98061d745
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-03 20:43:38 -0700

    Add files via upload

--- fopp/Chapter 4. Python Modules/n.n_times_table.py
+++ fopp/Chapter 4. Python Modules/n.n_times_table.py
@@ -1,1 +1,15 @@
+# The promgram prompts the user for one number.
+# It then displays the n*n times table.
 
+# Prompt user for one number
+num = int(input('Enter the number for the n*n times table: '))
+
+
+# Display n*n times table
+for a in range(1, num + 1):
+    for b in range(1, num + 1):
+        print(f'{b} x {a} = {b * a}', end = ' ')
+        if b == a:
+            print('')
+            break
+    
