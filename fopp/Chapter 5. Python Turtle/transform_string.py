28e1025887176a905528383d8a77682ffba840bc
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-04 18:28:12 -0700

    Add files via upload

--- fopp/Chapter 5. Python Turtle/transform_string.py
+++ fopp/Chapter 5. Python Turtle/transform_string.py
@@ -1,1 +1,12 @@
+# The program prompts the user for the string
+# It then changes "space" between words in string to "-",
+# and displays the new string with "-".
 
+# Prompt user for string
+string = input('Enter the string: ')
+
+# Change "space" to "-"
+result = '-'.join(string.split(' '))
+
+# Display new string with "-"
+print(f'The string "{string}" is transfromed into "{result}"')
