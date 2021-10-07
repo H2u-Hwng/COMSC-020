f077b08861324b7678864c213d1058c0e43de5e4
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-06 23:57:57 -0700

    Update payment.py

--- COMSC-020/Chapter 3. Debugging Interlude 1/payment.py
+++ COMSC-020/Chapter 3. Debugging Interlude 1/payment.py
@@ -8,23 +8,20 @@
 # Prompt user for principal
 principal = float(input('Enter amount of loan: $'))
 
-# Prompt user for annual interest rate as a percent 
-annual_rate = float(input('Enter annual interest rate as a percent: '))
+# Prompt user for annual interest rate as a percent and calculate annual rate
+annual_percent = float(input('Enter annual interest rate as a percent: '))
+annual_rate = annual_percent / 100
 
 # Prompt user for number of years for the loan
 years = int(input('Enter number of years for the loan: '))
 
-# Calculate monthly interest rate
-monthly_rate = (annual_rate / 12) / 100 
-
-# Calculate number of months of the loan
+# Calculate monthly interest rate, months of the loan, and subformula
+monthly_rate = annual_rate / 12
 months = years * 12
-
-# Common subformula
 subformula = (1 + monthly_rate)**months
 
 # Calculate monthly payment on the loan
 payment = principal * monthly_rate * subformula / (subformula - 1)
 
 # Print result
-print('Your monthly payment is ${:.2f}'. format(payment))
+print(f'Your monthly payment is ${payment:,.2f}')
