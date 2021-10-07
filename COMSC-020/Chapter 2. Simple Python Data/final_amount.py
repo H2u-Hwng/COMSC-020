cb6bbae793ab2fee6dbaa6de8ad57c84e9eedf3b
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-06 23:41:33 -0700

    Update final_amount.py

--- COMSC-020/Chapter 2. Simple Python Data/final_amount.py
+++ COMSC-020/Chapter 2. Simple Python Data/final_amount.py
@@ -1,24 +1,20 @@
 # Huu Hung Nguyen
 # 09/08/2021
 # final_amount.py
-# The program will assigns the principal amount, the number of times the interest is compounded per year, and the interest rate. 
-# The program will prompt the user for the number of years that the money will be compounded for. 
-# The program calculates the final amount and print it after t years.
+# Constants for principal, times the interest per year, and interest rate. 
+# The program prompts user for years that the money will be compounded for. 
+# It then calculates the final amount and print it after t years.
 
-# Principal amount
-P = 10000
-
-# Number of times the interest is compounded per year
-N = 12
-
-# Interest rate
-R = 0.08
+# Constants for principal, times the interest per year, and interest rate
+PRINCIPAL = 10000
+TIMES = 12
+INTEREST_RATE = 0.08
 
 # Prompt user for number of years
-year = int(input('Compound for how many years? '))
+years = int(input('Compound for how many years? '))
 
 # Calculate final amount
-final = P * ((1 + R / N)**(N * year)) 
+final_amount = PRINCIPAL * ((1 + INTEREST_RATE / TIMES)**(TIMES * years)) 
 
 # Display final amount
-print(f'The final amount after {year} years is ${final:.2f}.')
+print(f'The final amount after {years} years is ${final_amount:,.2f}.')
