ee134197d123608f3c81ebafdd03a767dec6bab7
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-12 18:51:13 -0700

    Add files via upload

---
+++ COMSC-020/Chapter 6. Functions/methods_demo.py
@@ -0,0 +1,15 @@
+# Print messages
+def message():
+    print('I am a CS programmer!')
+    print('Welcome to the world of programming.')
+    print('Please enjoy yourself.')
+    
+
+# Define main function
+def main():
+    print('I have a message for you.')
+    message()
+    print('Goodbye!')
+    
+# Call main function
+main()
