48c764e602b5ed1791aa92747ea1e03693578b1a
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-13 22:31:11 -0700

    Update donuts_method.py

--- COMSC-020/Chapter 6. Functions/donuts_method.py
+++ COMSC-020/Chapter 6. Functions/donuts_method.py
@@ -1,67 +1,62 @@
 # Huu Hung Nguyen
-# 10/12/2021
+# 10/13/2021
 # donuts_method.py
-# Constants for price dozen and single of both donuts.
-# The program prompts the user for the number of both donuts.
+# Constants for price dozen and single of plain and fancy donuts.
+# The program prompts the user for the number of plain and fancy donuts.
 # It then calculates and displays the number of dozens and single donuts
 # for each type of donuts, price of each type, and total cost.
 
 # Constants for price dozen and single of plain donuts
-PRICE_DOZEN_PLAIN = 7.00
-PRICE_SINGLE_PLAIN = 0.60
+COST_PLAIN_DOZEN = 7.00
+COST_PLAIN = 0.60
 
 # Constants for price dozen and single of fancy donuts
-PRICE_DOZEN_FANCY = 9.00
-PRICE_SINGLE_FANCY = 0.80
+COST_FANCY_DOZEN = 9.00
+COST_FANCY = 0.80
 
 # Constants for number dozen
 DOZEN = 12
 
-# Calculate and return number dozen and single of donuts
-def get_num_donuts(donuts):
+# Take the costs for dozens and singles along with the number of donuts
+# Calculate and return the number dozens, singles, and cost of donuts
+def get_donuts(cost_dozen, cost_single, donuts):
     
     # Calculate number dozen and single of donuts
     dozens = donuts // DOZEN
     singles = donuts % DOZEN
-
-    return dozens, singles
-
-
-# Calculate and return price donuts 
-def get_price(dozens, price_dozen, singles, price_single):
     
-    # Calculate price donuts
-    price = dozens * price_dozen + singles * price_single
-
-    return price
-
-
+    # Calculate cost donuts
+    cost = dozens * cost_dozen + singles * cost_single
+    
+    return dozens, singles, cost
+    
+ 
 # Define main function
 def main():
     
     # Prompt user for number of plain and fancy donuts
-    plain = int(input('Enter number of plain donuts you want to buy: '))
-    fancy = int(input('Enter number of fancy donuts you want to buy: '))
+    plain_donut = int(input('Enter number of plain donuts you want to buy: '))
+    fancy_donut = int(input('Enter number of fancy donuts you want to buy: '))
+   
+    # Obtain number dozens, singles, and cost of plain donuts
+    plain_dozens, plain_singles, cost_plain_donuts = \
+                  get_donuts(COST_PLAIN_DOZEN, COST_PLAIN, plain_donut)
     
-    # Obtain number dozen and single of plain and fancy donuts
-    dozens_plain, singles_plain = get_num_donuts(plain)
-    dozens_fancy, singles_fancy = get_num_donuts(fancy)
+    # Obtain number dozens, singles, and cost of fancy donuts
+    fancy_dozens, fancy_singles, cost_fancy_donuts = \
+                  get_donuts(COST_FANCY_DOZEN, COST_FANCY, fancy_donut)
     
-    # Obtain price of plain donuts, fancy donuts, and total cost
-    plain_cost = get_price(dozens_plain, PRICE_DOZEN_PLAIN,
-                           singles_plain, PRICE_SINGLE_PLAIN)
-    fancy_cost = get_price(dozens_fancy, PRICE_DOZEN_FANCY,
-                           singles_fancy, PRICE_SINGLE_FANCY)
-    total_cost = plain_cost + fancy_cost
+    # Obtain total cost
+    total_cost = cost_plain_donuts + cost_fancy_donuts
     
     # Display results
-    print(f'''Plain donuts:
-  {dozens_plain} dozen and {singles_plain} single
-  Cost: ${plain_cost:,.2f}
+    print(f'''Plain donuts: 
+   {plain_dozens} dozen and {plain_singles} single
+   Cost: ${cost_plain_donuts:,.2f}
 Fancy donuts:
-  {dozens_fancy} dozen and {singles_fancy} single
-  Cost: ${fancy_cost:,.2f}
+   {fancy_dozens} dozen and {fancy_singles} single
+   Cost: ${cost_fancy_donuts:,.2f}
 Total amount due: ${total_cost:,.2f}''')
-    
+
 # Call main function
 main()
