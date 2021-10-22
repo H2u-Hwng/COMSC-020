eb47a6380aa98a28f1db8f5f282e5ca857988049
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-21 19:32:10 -0700

    Add files via upload

--- COMSC-020/Chapter 7. Selection/payroll.py
+++ COMSC-020/Chapter 7. Selection/payroll.py
@@ -1,31 +1,39 @@
-# Constants
+# Huu Hung Nguyen
+# 10/20/2021
+# Nguyen_HuuHung_payroll.py
+# Constants for regular hours and overtime rate.
+# Program prompts user for hours and hourly rate.
+# It then calculates and displays the total wages for the week.
+
+# Constants for regular hours and overtime rate
 REGULAR_HOURS = 40
 OVERTIME_RATE = 1.5
 
-# Define wages function
-def wages(hours, hourly_rate):
+# Take hours and hourly rate, calculate and return total wages for the week
+def wages(hrs, hr_rate):
     
-    # Calculate total wages for the week
-    if hours <= REGULAR_HOURS:
-        total_wages = hourly_rate * hours 
+    # Calculate total wages for the week5
+    if hrs <= REGULAR_HOURS:
+        total_wages = hrs * hr_rate
     else:
-        overtime = hours - REGULAR_HOURS
-        total_wages = hourly_rate * (REGULAR_HOURS + OVERTIME_RATE * overtime)
-        
+        total_wages = REGULAR_HOURS * hr_rate + \
+                (hrs - REGULAR_HOURS) * OVERTIME_RATE * hr_rate
+    
     return total_wages
+
 
 # Define main function
 def main():
     
-    # Prompt user for hours worked and hourly rate
-    hours = float(input('Enter the number of hours worked: '))
+    # Prompt user for hours and hourly rate
+    hours = float(input('Enter the number of hours worked in the week: '))
     hourly_rate = float(input('Enter the hourly rate: '))
     
-    # Get total wages
+    # Obtain total wages
     total_wages = wages(hours, hourly_rate)
     
-    # Display total wages
-    print(f'The total wages is ${total_wages:,.2f}')
+    # Display result
+    print(f'Total wages for the week: ${total_wages:,.2f}')
     
 # Call main function
 main()
