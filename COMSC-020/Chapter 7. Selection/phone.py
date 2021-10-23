07e3a5e00a09dc2d57a79c4c7159939613690e17
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-22 19:37:31 -0700

    Add files via upload

--- COMSC-020/Chapter 7. Selection/phone.py
+++ COMSC-020/Chapter 7. Selection/phone.py
@@ -1,60 +1,87 @@
-# Constants for prices, regular units, and extra money of two plans
-PRICE_1 = 9.38
-LIMIT_UNITS_1 = 65
-EXTRA_MONEY_1 = 0.045
+# Huu Hung Nguyen
+# 10/22/2021
+# Nguyen_HuuHung_phone.py
+# Constants for base prices, extra prices, and limit units of both plans
+# Program prompts user for number of units used (integer).
+# It calculates and displays the cost for the units using both plans.
+# It then tells which plan is cheaper.
 
-PRICE_2 = 8.57
-LIMIT_UNITS_2 = 50
-EXTRA_MONEY_2 = 0.052
+# Constants for base prices, extra prices, and limit units of both plans
+PRICE1 = 9.38
+EXTRA_PRICE1 = 0.045
+LIMIT_UNITS1 = 65
+PRICE2 = 8.57
+EXTRA_PRICE2 = 0.052
+LIMIT_UNITS2 = 50
 
-# Define get units function
+# Prompt and return number of units used 
 def get_units():
+    valid = False
     
-    # Prompt user for the number of units used
-    units = int(input('Enter number of units used, or zero to quit: '))
+    # Enter units until valid units are entered
+    while not valid:
+        units = int(input('Enter number of units used, or -1 to finish: '))
+        
+        # Check if valid units are entered
+        if units >= 0 or units == -1:
+            valid = True
+        else:
+            print('You cannot have negative units.')
+            print('Please enter valid units. \n')
     
     return units
 
-# Define calculate cost function
-def calculate_cost(units, price, limit_units, extra_money):
+
+# Calculate total cost 
+def calculate_cost(units, cost, base_limit, extra_cost):
     
-    # Calculate cost for units using the plan
-    total_cost = price
-    if units > limit_units:
-        extra_units = units - limit_units
-        total_cost += extra_units * extra_money
+    # Initialize total cost
+    total_cost = cost
+    
+    # Update the cost for extra units
+    if units > base_limit:
+        total_cost += (units - base_limit) * extra_cost
         
     return total_cost
 
+
+# Display cheaper plan
+def check_plans(total_cost1, total_cost2):
+    if total_cost1 < total_cost2:
+        print('Plan 1 is cheaper. \n')
+    else:
+        print('Plan 2 is cheaper. \n')
+     
+     
 # Define main function
-def main(units):
-    
-    # Get cost for units using the first plan
-    total_cost_1 = calculate_cost(units, PRICE_1, LIMIT_UNITS_1, EXTRA_MONEY_1)
-    
-    # Get cost for units using the second plan
-    total_cost_2 = calculate_cost(units, PRICE_2, LIMIT_UNITS_2, EXTRA_MONEY_2)
-    
-    # Display cost of two plans and tell which plan is cheaper
-    print(f'Cost for plan 1: ${total_cost_1:,.2f}')
-    print(f'Cost for plan 2: ${total_cost_2:,.2f}')
-    
-    if total_cost_1 < total_cost_2:
-        print(f'Plan 1 is cheaper. \n')
-    else:
-        print(f'Plan 2 is cheaper. \n')
+def main():
         
-# Get results
-while True:
+    # Keep entering units until finish is set to true
+    finish = False
     
-    # Get units
-    units = get_units()
+    # Enter units until the user enters -1
+    while not finish:
+        
+        # Obtain the number of units used
+        units_used = get_units()
+        
+        # Check if user finish entering units
+        if units_used == -1:
+            finish = True
+        else:
+            
+            # Obtain the cost for the units using the first and second plans
+            total_price1 = calculate_cost(units_used, PRICE1,
+                                          LIMIT_UNITS1, EXTRA_PRICE1)
+            total_price2 = calculate_cost(units_used, PRICE2,
+                                          LIMIT_UNITS2, EXTRA_PRICE2)
+                
+            # Display the cost for the units using the first and second plans
+            print(f'Cost for plan 1: ${total_price1:,.2f}')
+            print(f'Cost for plan 2: ${total_price2:,.2f}')
+                
+            # Display cheaper plan
+            check_plans(total_price1, total_price2)
     
-    # Call main function
-    if units < 0:
-        print('You cannot have negative units.')
-    elif units == 0:
-        break
-    else:
-        main(units)
-    
+# Call main function
+main()
