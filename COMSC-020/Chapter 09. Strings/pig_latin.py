4b097b8f3053787ecdb5ed54971815c56b9e7696
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-11-16 19:18:37 -0800

    Update pig_latin.py

--- COMSC-020/Chapter 09. Strings/pig_latin.py
+++ COMSC-020/Chapter 09. Strings/pig_latin.py
@@ -1,39 +1,42 @@
 # Huu Hung Nguyen
-# 11/10/2021
+# 11/12/2021
 # Nguyen_HuuHung_pig_latin.py
 # Program prompts user for a sentence.
 # Program contains function converting sentence to pig latin.
 # It then displays result.
 
-# Constants for vowels
-VOWELS = "aeiouyAEIOUY"
+# Constant for vowels
+VOWELS = 'aeiouyAEIOUY'
 
-# Convert word to pig latin and return new word
 def to_pig_latin(word):
+    '''Take a word and covert to its pig latin
+       Return pig latin word.'''
     
     # Initialize position
     position = 0
     
     # Check if the first letter of word is in vowels
     if word[0] in VOWELS:
-        # Check if first letter of word is Y or y
-        if word[0] in "yY":
+        # Check if the first letter of word is y or Y
+        if word[0] in 'yY':
             position += 1
             
             # Add 1 to position until finding position of vowel in word
             while word[position] not in VOWELS and position < len(word):
                 position += 1
+            
             new_word = word[position:] + word[:position] + 'ay'
         else:
-            new_word = word + "way" 
+            new_word = word + 'way'
     else:
         # Check if first 2 letters of word is qu or Qu
-        if word[:2] in "quQu":
+        if word[:2] in 'quQu':
             position += 2
             
         # Add 1 to position until finding position of vowel in word
         while word[position] not in VOWELS and position < len(word):
             position += 1
+            
         new_word = word[position:] + word[:position] + 'ay'
     
     # Check if the first letter of word is uppercase
@@ -43,20 +46,21 @@ def to_pig_latin(word):
     return new_word
       
       
-# Define main function
 def main():
+    '''Define main function.'''
+    
     again = True
     
-    # Convert sentence to pig latin until users enter a new line
+    # Convert sentence to its pig latin until users enter a new line
     while again:
         
-        # Get a sentence from the user
+        # Prompt the user for a sentence
         sentence = input('Please enter a sentence: ')
         
-        # Split the sentence into a list of words
+        # Split words of sentence into the words list
         words_list = sentence.split()
         
-        # Create result
+        # Initialize pig latin translation
         pig_sentence = ''
         
         # Check if users enter a new line
@@ -64,12 +68,13 @@ def main():
             again = False
         else:
             for word in words_list:
-                # Convert word to pig latin
+                # Convert word to pig latin word
                 pig_word = to_pig_latin(word)
-                # Obtain result
+                
+                # Determine pig latin sentence
                 pig_sentence += pig_word + ' '
             
-            # Display result
+            # Display pig latin translation
             print(pig_sentence)
         
 # Call main function
