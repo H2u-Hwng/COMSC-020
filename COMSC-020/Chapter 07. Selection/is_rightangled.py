1f12e2d003a26ca4dcc496713439d42a61897557
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-11-16 20:24:25 -0800

    Update is_rightangled.py

--- COMSC-020/Chapter 07. Selection/is_rightangled.py
+++ COMSC-020/Chapter 07. Selection/is_rightangled.py
@@ -1,8 +1,20 @@
+def sum_squared_sides(x, y):
+    '''Take two sides of a triangle and calculate the sum squared sides
+       Return sum squared sides.'''
+    
+    sum_squared = x**2 + y**2
+    
+    return sum_squared
+    
+    
 def is_rightangled(x, y, z):
     '''Take three sides of a triangle
        Return True if the triangle is right-angled and False otherwise.'''
-
-    if abs(z**2 - (x**2 + y**2)) < 0.001:
+    
+    # Determine min side, side, and max side of the triangle
+    min_side, side, max_side = sorted([x, y, z])
+    
+    if abs(max_side**2 - sum_squared_sides(min_side, side)) < 0.001:
         rightangled = True
     else:
         rightangled = False
@@ -16,7 +28,7 @@ def main():
     # Prompt the user for three sides of the triangle
     side1 = float(input('Please enter the first side of the triangle: '))
     side2 = float(input('Please enter the second side of the triangle: '))
-    side3 = float(input('Please enter the longest side of the triangle: '))
+    side3 = float(input('Please enter the third side of the triangle: '))
     
     # Determine whether the triangle is right-angled
     print(is_rightangled(side1, side2, side3))
