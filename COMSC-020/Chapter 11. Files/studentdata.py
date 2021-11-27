83f25184705317e7aed66fc57b38fd9622fe2ab2
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-11-26 20:04:54 -0800

    Update studentdata.py

--- COMSC-020/Chapter 11. Files/studentdata.py
+++ COMSC-020/Chapter 11. Files/studentdata.py
@@ -1,12 +1,63 @@
-# Open file
-student_data = open('studentdata.txt', 'r')
+# Constant for file name
+INPUT_FILE = 'studentdata.txt'
 
-for name_scores in student_data:
-    name_scores_list = name_scores.split()
-    # Check if students have more than six quiz scores
-    if len(name_scores_list[1:]) > 6:
-        # Display names of students having more than six quiz scores
-        print(name_scores_list[0])
+def get_mean(data_list):
+    ''' Take a data list and calculate its mean
+        Return the mean of data list. '''
+    
+    # Determine the number having in the data list
+    n = len(data_list)
+    
+    # Determine the sum of data in the data list
+    sum_data = sum(data_list)
+    
+    # Check whether the data list is empty
+    if n <= 0:
+        mean = 0
+    else:
+        # Calculate the mean of the data list
+        mean = sum_data / n
+    
+    return mean
+    
+    
+def find_quantity(data_list):
+    ''' Take a data list
+        Return True if its length is larger than 6 and False otherwise. '''
+    
+    quantity = len(data_list) > 6
         
-# Close file
-student_data.close()
+    return quantity
+        
+    
+def main():
+    ''' Define main function. '''
+    
+    # Open file and automatically close it when being done
+    with open(INPUT_FILE, 'r') as input_file:
+        for line in input_file:
+            # Strip whitespace off the end and split student and scores
+            student_scores = line.strip().split()
+            
+            # Determine the student's name
+            student = student_scores[0]
+            
+            # Determine the scores list 
+            scores = []
+            for num in student_scores[1:]:
+                scores.append(float(num))
+            
+            # Display student, minimum, maximum, and average
+            print(f'Name: {student.capitalize()}')
+            print(f'Minimum score: {min(scores)}; Maximum score: {max(scores)}')
+            print(f'Average score: {get_mean(scores):,.3f}')
+            
+            # Check whether the student has more than six scores
+            if find_quantity(scores):
+                print('Having more than six quiz scores: YES')
+            else:
+                print('Having more than six quiz scores: NO')
+            print()
+            
+# Call main function
+main()
