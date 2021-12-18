b257c4ba168002c79e70d811343c2be446eb1baf
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-17 22:56:19 -0800

    Create HelloWorld.java

---
+++ COMSC-075/Chapter 01. Computer Programming/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Demonstrate Java output.
+ */
+
+public class Hello {
+    
+    public static void main(String[] args) {
+        // Generate some simple output
+        System.out.println("Hello, World!");
+    }
+}
