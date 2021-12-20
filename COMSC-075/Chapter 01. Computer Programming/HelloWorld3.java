d8b1ba7b473e63e898c88af1bf369ec3f9c70495
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-20 15:40:54 -0800

    Create HelloWorld3.java

---
+++ COMSC-075/Chapter 01. Computer Programming/HelloWorld3.java
@@ -0,0 +1,7 @@
+public class Hello3 {
+    
+    public static void main(String[] args) {
+        System.out.println("She said \"Hello!\" to me.");
+        System.out.println("Hello! \nHow are you doing?");
+    }
+}
