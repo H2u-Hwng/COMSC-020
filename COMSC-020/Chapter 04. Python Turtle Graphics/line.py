97e3c0e4d4aba6267d9d3d79a32126cc8fe2ea9b
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-23 18:42:57 -0800

    Create line.py

---
+++ COMSC-020/Chapter 04. Python Turtle Graphics/line.py
@@ -0,0 +1,17 @@
+# Library
+import turtle
+
+# Create window
+wn = turtle.Screen()
+
+# Create turtle named figure
+figure = turtle.Turtle()
+figure.hideturtle()
+
+# Draw figure
+figure.forward(150)
+figure.left(90)
+figure.forward(50)
+
+# Close window
+wn.exitonclick()
