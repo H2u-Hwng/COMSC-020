1988cb63b024bff439abc9bffb2b96aac07ad144
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-23 20:20:44 -0800

    Add files via upload

---
+++ COMSC-020/Chapter 02. Simple Python Data/giftwrap.py
@@ -0,0 +1,16 @@
+# Gift Wrap
+# Your name, today's date
+#
+# If you are wrapping a rectangular box in paper, what is the required
+# surface area if there's a 5% overlap?
+#
+# 1) Ask for the length of the box
+# 2) Ask for the width of the box
+# 3) Ask for the height of the box
+# 4) Calculate the area without overlap as:
+#    2 * ((length * width) + (length * height) + (width * height))
+# 5) Calculate the area with overlap as 1.05 times the area without overlap.
+# 6) Display the area with overlap, properly labeled.
+#
+# You may choose any unit of measurement for the dimensions that you like (inches,
+# centimeters, whatever).
