d256ab3ba33a5f8dd9f57ea944f33cc927a94412
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-23 20:03:04 -0800

    Add files via upload

---
+++ COMSC-020/Chapter 03. Debugging Interlude 1/cinema.py
@@ -0,0 +1,20 @@
+#
+#   Your program will repeatedly ask for a day of the week
+#   where 1 = Monday and 7 = Sunday
+#
+#   It will then ask for an hour of the day (0-23)
+#   It then displays a price for a movie ticket, based on the
+#   following table:
+#
+#   Monday and Wednesday all day: $5.00
+#   All other days before 1 p.m.: $6.00
+#   All other times and days: $8.00
+#   Hint: if/elif/....
+#
+#   After printing the ticket price, it will ask if you want to
+#   keep going.
+#
+#   Your program must make sure that the day is from 1-7 and that
+#   the hour is from 0-23. Hint: write a get_day() and get_hour()
+#   function. Each of these functions will have a while loop to make
+#   sure that input is OK.
