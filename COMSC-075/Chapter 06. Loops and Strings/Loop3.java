9f300437ad6f81029996a9dbbf1728527c026656
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-05 21:52:59 -0800

    Create Loop3.java

---
+++ COMSC-075/Chapter 06. Loops and Strings/Loop3.java
@@ -0,0 +1,13 @@
+public class Loop3 {
+    
+    public static void main(String[] args) {
+        
+        int n; // initialize a number
+        
+        for (n = 5; n >= 0; n --) {
+            System.out.println(n);
+        }
+        
+        System.out.println("n is now " + n);
+    }
+}
