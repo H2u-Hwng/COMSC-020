7b50a56e0efe4cd80a73ef6ad87afaffcac38bc8
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-08 10:59:27 -0800

    Create Alphabet.java

---
+++ COMSC-075/Chapter 06. Loops and Strings/Alphabet.java
@@ -0,0 +1,12 @@
+public class Alphabet {
+    
+    public static void main(String[] args) {
+    
+        System.out.print("Roman alphabet: ");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c);
+        }
+        
+        System.out.println();
+    }
+}
