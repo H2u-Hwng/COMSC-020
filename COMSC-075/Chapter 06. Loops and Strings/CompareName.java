c650fe7356a333011514cf9025dfd97bf71b1de3
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-11 17:37:03 -0800

    Create CompareName.java

---
+++ COMSC-075/Chapter 06. Loops and Strings/CompareName.java
@@ -0,0 +1,17 @@
+public class CompareName {
+    
+    public static void main(String[] args) {
+        String name1 = "Alan Turing";
+        String name2 = "Ada lovelace";
+        
+        int diff = name1.compareTo(name2);
+        
+        if (diff < 0) {
+            System.out.println(name1 + " comes before " + name2);
+        } else if (diff > 0) {
+            System.out.println(name2 + " comes before " + name1);
+        } else {
+            System.out.println("The names are the same!");
+        }
+    }
+}
