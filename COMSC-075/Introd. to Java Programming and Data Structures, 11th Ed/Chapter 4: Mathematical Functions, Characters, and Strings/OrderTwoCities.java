9c98279d85c01e3b7bb384a397be889adf29515f
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-02-22 23:33:16 -0800

    Add files via upload

---
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 4: Mathematical Functions, Characters, and Strings/OrderTwoCities.java
@@ -0,0 +1,23 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class OrderTwoCities {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		    
+		// Prompt the user to enter two cities
+		System.out.print("Enter the first city: ");
+		String city1 = input.nextLine();
+		
+		System.out.print("Enter the second city: ");
+		String city2 = input.nextLine();
+		
+		// Compare two cities and display them in alphabetical order
+		if (city1.compareTo(city2) < 0)
+			System.out.printf("The cities in alphabetical order are %s, %s.", city1, city2);
+		else
+			System.out.printf("The cities in alphabetical order are %s, %s.", city2, city1);
+	}
+}
