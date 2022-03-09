fe9094780f9d3af18fd4cb823c6571d22caf5f25
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-03-08 23:47:02 -0800

    Add files via upload

--- COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 04: Mathematical Functions, Characters, and Strings/Circle.java
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 04: Mathematical Functions, Characters, and Strings/Circle.java
@@ -1,12 +1,13 @@
 public class Circle {
 
-  public static double computeArea(double radius) {
-    return Math.PI * Math.pow(radius, 2);
-  }
-  
-  public static void main(String[] args) {
-    double diameter = 10.0;
-    double area = computeArea(diameter / 2);
-    System.out.printf("The circle's area having diaemter %.3f is %.3f", diameter, area);
-  }
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        double diameter = 10.0;
+        double area = calculateArea(diameter / 2);
+        System.out.println(area);
+    }
+
 }
