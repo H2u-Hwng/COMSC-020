26a304c36179e414e33fbe9baec6427b1a73544c
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-03-09 21:08:20 -0800

    Update Distance.java

--- COMSC-075/Chapter 03. Input and Output/Distance.java
+++ COMSC-075/Chapter 03. Input and Output/Distance.java
@@ -7,16 +7,16 @@ public class Distance {
 		Scanner input = new Scanner(System.in);
     
 		System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
-	    double x1 = input.nextDouble();
-	    double y1 = input.nextDouble();
+	    	double x1 = input.nextDouble();
+	    	double y1 = input.nextDouble();
 	    
-	    System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
-	    double x2 = input.nextDouble();
-	    double y2 = input.nextDouble();    
+	    	System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
+	    	double x2 = input.nextDouble();
+	    	double y2 = input.nextDouble();    
 	    
-	    double d = 6371.01 * Math.acos(Math.sin(Math.toRadians(x1)) * Math.sin(Math.toRadians(x2)) +
-	      Math.cos(Math.toRadians(x1)) * Math.cos(Math.toRadians(x2)) * 
-	      Math.cos(Math.toRadians(y1 - y2)));
-	    System.out.println("The distance between the two points is " + d + " km");
-  }
+	    	double d = 6371.01 * Math.acos(Math.sin(Math.toRadians(x1)) * Math.sin(Math.toRadians(x2)) +
+	      			Math.cos(Math.toRadians(x1)) * Math.cos(Math.toRadians(x2)) * 
+	     			Math.cos(Math.toRadians(y1 - y2)));
+	    	System.out.println("The distance between the two points is " + d + " km");
+  	}
 }
