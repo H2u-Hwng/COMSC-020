7d6fc98c26ce51c17721e81fddfcf5f6d065906d
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-03-19 23:23:26 -0700

    Add files via upload

---
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 06: Methods/Format.java
@@ -0,0 +1,34 @@
+/**
+ * Example using the String.format method.
+ */
+public class Format {
+
+    /**
+     * Returns a time string in 12-hour format.
+     *
+     * @param hour between 0 and 23
+     * @param minute between 0 and 59
+     */
+    public static String timeString(int hour, int minute) {
+        String ampm;
+        if (hour < 12) {
+            ampm = "AM";
+            if (hour == 0) {
+                hour = 12;  // midnight
+            }
+        } else {
+            ampm = "PM";
+            hour = hour - 12;
+        }
+        return String.format("%02d:%02d %s", hour, minute, ampm);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeString(0, 0));
+        System.out.println(timeString(7, 30));
+        System.out.println(timeString(9, 30));
+        System.out.println(timeString(19, 50));
+        System.out.println(timeString(24, 59));
+    }
+
+}
