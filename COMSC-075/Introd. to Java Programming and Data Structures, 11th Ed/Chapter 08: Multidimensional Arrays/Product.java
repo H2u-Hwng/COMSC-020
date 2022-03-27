6d85259fd6939347c7327fd6f9f30e6a38bad2b1
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-03-26 23:37:05 -0700

    Create Product.java

---
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 08: Multidimensional Arrays/Product.java
@@ -0,0 +1,22 @@
+/**
+ * Stack diagram exercise.
+ */
+
+public class Product {
+	public static int prod(int m, int n) {
+		int result;
+				
+		if (m == n) {
+		result = n;
+		} else {
+		int recurse = prod(m, n - 1);
+		result = n * recurse;
+		}
+		        
+		return result;
+	}
+		
+	public static void main(String[] args) {
+		System.out.println(prod(1, 4));
+	}
+}
