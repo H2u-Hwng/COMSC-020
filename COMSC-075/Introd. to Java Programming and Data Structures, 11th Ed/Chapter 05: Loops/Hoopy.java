11ad5344fccf20b0587de0148cecfaee500ef0ae
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-05 16:57:01 -0700

    Add files via upload

--- COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 05: Loops/Hoopy.java
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 05: Loops/Hoopy.java
@@ -1,36 +1,26 @@
 public class Hoopy {
 
-    public static void main(String[] args) {
-        boolean flag1 = isHoopy(202);
-        boolean flag2 = isFrabjuous(202);
-        System.out.println(flag1);
-        System.out.println(flag2);
-        if (flag1 && flag2) {
-            System.out.println("ping!");
-        }
-        if (flag1 || flag2) {
-            System.out.println("pong!");
-        }
-    }
+	public static void main(String[] args) {
+		boolean flag1 = isHoopy(201);
+		boolean flag2 = isFrabjuous(202);
+		System.out.println(flag1);
+		System.out.println(flag2);
+		if (flag1 && flag2) {
+			System.out.println("ping!");
+		}
+		if (flag1 || flag2) {
+			System.out.println("pong!");
+		}
+	}
 
-    public static boolean isHoopy(int x) {
-        boolean hoopyFlag;
-        if (x % 2 == 0) {
-            hoopyFlag = true;
-        } else {
-            hoopyFlag = false;
-        }
-        return hoopyFlag;
-    }
+	public static boolean isHoopy(int x) {
+		boolean hoopyFlag = (x % 2 == 0);
+		return hoopyFlag;
+	}
 
-    public static boolean isFrabjuous(int x) {
-        boolean frabjuousFlag;
-        if (x > 0) {
-            frabjuousFlag = true;
-        } else {
-            frabjuousFlag = false;
-        }
-        return frabjuousFlag;
-    }
+	public static boolean isFrabjuous(int x) {
+		boolean frabjuousFlag = (x > 0);
+		return frabjuousFlag;
+	}
 
 }
