a2c61e3a01cf810cd466f53cfcbeb84abc6dd953
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-04-06 22:30:37 -0700

    Add files via upload

--- COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 05: Loops/IfElse.java
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 05: Loops/IfElse.java
@@ -1,15 +1,44 @@
 public class IfElse {
 
-    public static void main(String[] args) {
-        int x = 17;
-        int n = 18;
+	public static void main(String[] args) {
+		int x = 0;
+		int n = -17;
 
-        if (x % 2 == 0) System.out.println("x is even");
-        else System.out.println("x is odd");
+		if (x == 0) {
+			System.out.println("x is zero");
+		} else {
+			System.out.println("x is not zero");
+			
+			if (x > 0) {
+				System.out.println("x is positive");
+			} else {
+				System.out.println("x is negative");
+			}
+		}
+			
+		if (x % 2 == 0) {
+			System.out.println("x is even");
+		} else {
+			System.out.println("x is odd");
+		}
 
-        if (n > 0) {
-            System.out.println("x is positive");
-            System.out.println("x is not zero");
-        }
-    }
+		if (n == 0) {
+			System.out.println("n is zero");
+		} else {
+			System.out.println("n is not zero");
+			
+			if (n > 0) {
+				System.out.println("n is positive");
+			} else {
+				System.out.println("n is negative");
+			}
+		}
+			
+		if (n % 2 == 0) {
+			System.out.println("n is even");
+		} else {
+			System.out.println("n is odd");
+		}
+	}
+
 }
