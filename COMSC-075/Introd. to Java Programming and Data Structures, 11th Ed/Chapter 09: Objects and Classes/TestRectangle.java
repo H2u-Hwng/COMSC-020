566fcee0851d6fe03a830f1bd6b86b15571d1a09
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-08-10 23:30:54 -0700

    Update TestRectangle.java

--- COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 09: Objects and Classes/TestRectangle.java
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 09: Objects and Classes/TestRectangle.java
@@ -3,11 +3,17 @@ public class TestRectangle {
 
 	public static void main(String[] args) {
 		Rectangle r1 = new Rectangle();
-		Rectangle r2 = new Rectangle(3.0, 5.5);
-		
 		double area1 = r1.getArea();
 		double perimeter1 = r1.getPerimeter();
 		System.out.printf("area: %,.3f perimeter: %,.3f\n", area1, perimeter1);
+		
+		Rectangle r2 = new Rectangle(3.0, 5.5);
 		r2.show();
+		
+		Rectangle r3 = new Rectangle(4, 40);
+        	r3.show();
+        
+        	Rectangle r4 = new Rectangle(3.5, 35.9);
+        	r4.show();
 	}
 }
