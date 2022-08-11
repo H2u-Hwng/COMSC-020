fe756ea9d91f0e386db32f2ebf29818151c57719
Huu Hung Nguyen <79944868+hhnguyen2000@users.noreply.github.com>
2022-08-10 23:35:01 -0700

    Update Rectangle.java

--- COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 09: Objects and Classes/Rectangle.java
+++ COMSC-075/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 09: Objects and Classes/Rectangle.java
@@ -1,27 +1,38 @@
-
 public class Rectangle {
-	
-	private double width, height; // properties
-	
-	public Rectangle() {
-		width = 1.0;
-		height = 1.0;
-	}
-	
-	public Rectangle(double w, double h) {
-		width = w;
-		height = h;
-	}
-	
-	public double getArea() {
-		return width * height;
-	}
-	
-	public double getPerimeter() {
-		return 2.0 * (width + height);
-	}
-	
-	public void show() {
-		System.out.printf("area: %,.3f perimeter: %,.3f\n", getArea(), getPerimeter());
-	}
+    
+    // properties
+    private double width = 1.0;
+    private double height = 1.0;
+    
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public Rectangle() {
+        this(1.0, 1.0);
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public double getArea() {
+        return width * height;
+    }
+    
+    public double getPerimeter() {
+        return (width + height) * 2.0;
+    }
+    
+    public void show() {
+        System.out.printf(
+            "The area of a %.1f x %.1f Rectangle is %.2f\n", width, height, getArea());
+        System.out.printf(
+            "The perimeter of a %.1f x %.1f Rectangle is %.2f\n", width, height, getPerimeter());
+    }
 }
