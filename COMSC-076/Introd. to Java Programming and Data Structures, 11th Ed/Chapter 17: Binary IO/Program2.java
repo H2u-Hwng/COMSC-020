8d45a242d2c226bdd75051f5607d8adf1c070dd3
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-08 22:58:29 -0700

    Update Program2.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 17: Binary IO/Program2.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 17: Binary IO/Program2.java
@@ -1,10 +1,10 @@
-import java.io.*
+import java.io.*;
 
-public class Program2 {
+class Program2 {
     public static void main(String[] args) throws Exception {
-        File file = new File("testOutput.dat")
+        File file = new File("testOutput.dat");
         
-        BitOutputStream output = new BitOutputStream(file;
+        BitOutputStream output = new BitOutputStream(file);
         output.writeBit("010000100100001001101");
         output.close();
         System.out.println("Done");
@@ -12,11 +12,14 @@ public class Program2 {
 
     public static class BitOutputStream {
         private FileOutputStream output;
-
+        private int val;
+        private int count = 0;
+        private int mask = 1;
+        
         // Creates a BitOutputStream to write bits to the file.
         public BitOutputStream(File file) throws IOException {
             output = new FileOutputStream(file);
-        
+        }
         // Writes a string of bits to the output stream
         public void writeBit(String bitString) throws IOException {
             for (int i = 0; i < bitString.length(); i++) {
@@ -26,15 +29,27 @@ public class Program2 {
 
         // Writes a bit '0' or '1' to the output stream
         public void writeBit(char bit) throws IOException {
-            output.write(bit);
-      }
+            count++;
+            val = val << 1;
+            if (bit == '1') {
+                val = val | mask;
+            }
+            
+            if (count == 8) {
+                output.write(val);
+                count = 0;
+            }
+        }
 
      /** Write the last byte and close the stream. If the last byte is not full, right-shfit with zeros         */
-     public void close() throws IOException {
-         // Program statements for this method
+        public void close() throws IOException {  
+         // Check whether the bit values are greater // than 0 or not
 
-
-          output.close();  // This makes use of the close() method for a FileOutputStream object
-       }
-   }
+            if (count > 0) {
+                val = val << (8 - count);
+                output.write(val);
+            }
+            output.close();  // This makes use of the close() method for a FileOutputStream object
+        }
+    }
 }
