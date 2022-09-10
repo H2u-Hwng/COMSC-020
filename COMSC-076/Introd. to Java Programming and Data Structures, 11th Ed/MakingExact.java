cc718dc66377695e742577520859ad6171f2878e
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-09 21:20:51 -0700

    Create MakingExact.java

---
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/MakingExact.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class MakingExact {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Money = $");
+        double money = input.nextDouble();
+        
+        double[] bills = {100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.01};
+        
+        
+        
+        for (double n: bills) {
+            if (money > n) {
+                int count = (int) (money / n);
+               
+                if (money >= 1 && count > 1) {
+                    System.out.printf("%d $%.0f bills \n", count, n);
+                } else if (money >= 1) {
+                    System.out.printf("%d $%.0f bill \n", count, n);
+                } else if (money < 1 && count > 1) {
+                   System.out.printf("%d %.0f-cent coins \n", count, n*100);
+                } else {
+                    System.out.printf("%d %.0f-cent coin \n", count, n*100);
+                }
+            }
+            
+            money %= n;
+        }
+    }
+}
