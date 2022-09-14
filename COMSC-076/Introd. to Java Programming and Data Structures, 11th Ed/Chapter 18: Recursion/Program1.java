822fc955e99803ed090c46f5d9e9a0074688a6b4
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-13 17:04:18 -0700

    Update Program1.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
@@ -7,8 +7,21 @@ public class Program1 {
         
         System.out.print("Enter your string: ");
         String value = input.nextLine();
+        
+        reverseDisplay(value, value.length());
     }
     
-    public static void reverseDisplay(String value) {}
-    public static void reverseDisplay(String value, int high) {}
+    public static void reverseDisplay(String value) {
+        reverseDisplay(value, value.length());
+    }
+    
+    public static void reverseDisplay(String value, int high) {
+        String result = "";
+        
+        for (int i = high - 1; i > -1; i --) {
+            result += String.valueOf(value.charAt(i));
+        }
+        
+        System.out.println(result);
+    }
 }
