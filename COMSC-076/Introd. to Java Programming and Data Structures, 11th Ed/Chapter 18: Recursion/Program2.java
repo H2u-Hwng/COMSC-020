5ee3519cccd66ee01baa513042b2d0723da057bc
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-14 00:16:38 -0700

    Create Program2.java

---
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program2 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your string: ");
+        String s = input.nextLine();
+    }
+    
+    public static void displayPermutation(String s) {
+        displayPermutation(" ", s);
+    }
+        
+    public static void displayPermutation(String s1, String s2) {}
+    
+}
