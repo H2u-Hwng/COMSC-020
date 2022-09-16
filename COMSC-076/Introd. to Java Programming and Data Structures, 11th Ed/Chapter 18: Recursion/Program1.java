3232d140e9bf16cf40b4659014b5c3edd0841053
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-15 17:20:36 -0700

    Update Program1.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
@@ -1,27 +1,38 @@
+/*
+ * Huu Hung Nguyen
+ * 09/15/2022
+ * Assignment 3
+ * Write a recursive method that displays a string in reverse order.
+ * The program has the method call a helper method 
+ * that includes the string's high index.
+ */
+
 import java.util.Scanner;
 
 public class Program1 {
-    
+    /** Main method */
     public static void main(String[] args) {
+        // Create a Scanner
         Scanner input = new Scanner(System.in);
         
+        // Prompt the user for a string
         System.out.print("Enter your string: ");
         String value = input.nextLine();
         
-        reverseDisplay(value, value.length());
+        // Display reversed string
+        reverseDisplay(value);
     }
     
+    /** Method for displaying a string in reverse order */
     public static void reverseDisplay(String value) {
         reverseDisplay(value, value.length());
     }
     
+    /** Helper method including the string's high index */
     public static void reverseDisplay(String value, int high) {
-        String result = "";
-        
-        for (int i = high - 1; i > -1; i --) {
-            result += String.valueOf(value.charAt(i));
+        if (high > 0) {
+            System.out.print(value.charAt(high - 1));
+            reverseDisplay(value.substring(0, high - 1));
         }
-        
-        System.out.println(result);
     }
 }
