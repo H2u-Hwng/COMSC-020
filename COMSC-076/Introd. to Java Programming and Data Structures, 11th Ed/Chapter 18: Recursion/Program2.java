ef2e335a6e88c65c1531cbfa89f32cd04cc7fe9f
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-16 21:05:23 -0700

    Update Program2.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program2.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program2.java
@@ -1,18 +1,44 @@
+/*
+ * Huu Hung Nguyen
+ * 09/16/2022
+ * Assignment 3
+ * Write a recursive method to print all permutations of a string.
+ * write a program prompting the user to enter a string of characters,
+ * and displays all of its permutations.
+ */
+
 import java.util.Scanner;
 
 public class Program2 {
-    
+    /** Main method */
     public static void main(String[] args) {
+        // Create a scanner
         Scanner input = new Scanner(System.in);
         
+        // Prompt the user for a string
         System.out.print("Enter your string: ");
         String s = input.nextLine();
+        
+        // Display the permutation
+        displayPermutation(s);
     }
     
+    /** Recursive method to print all permutations of a string */
     public static void displayPermutation(String s) {
-        displayPermutation(" ", s);
+        displayPermutation("", s);
     }
         
-    public static void displayPermutation(String s1, String s2) {}
-    
+    /** Helper method to move a character from s2 to s1 
+        and recursively invokes it with new s1 and s2 */
+    public static void displayPermutation(String s1, String s2) {
+        int len = s2.length();
+        
+        if (len > 0) {
+            for (int i = 0; i < len; i ++) {
+                displayPermutation(s1 + s2.charAt(i), s2.substring(0, i) + s2.substring(i + 1));
+            }
+        } else {
+            System.out.println(s1);
+        }
+    }
 }
