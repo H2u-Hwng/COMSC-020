5c51ebf68ec493f61f3ed96de7a3d234eaba9fcf
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-16 21:03:40 -0700

    Update Program1.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 18: Recursion/Program1.java
@@ -12,23 +12,23 @@ import java.util.Scanner;
 public class Program1 {
     /** Main method */
     public static void main(String[] args) {
-        // Create a Scanner
+        // Create a scanner
         Scanner input = new Scanner(System.in);
         
         // Prompt the user for a string
         System.out.print("Enter your string: ");
         String value = input.nextLine();
         
-        // Display reversed string
+        // Display the reversed string
         reverseDisplay(value);
     }
     
-    /** Method for displaying a string in reverse order */
+    /** Recursive method to display a string in reverse order */
     public static void reverseDisplay(String value) {
         reverseDisplay(value, value.length());
     }
     
-    /** Helper method including the string's high index */
+    /** Helper method to include the string's high index */
     public static void reverseDisplay(String value, int high) {
         if (high > 0) {
             System.out.print(value.charAt(high - 1));
