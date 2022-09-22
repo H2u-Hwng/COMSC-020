57aeec3dace9782fcc9dad9b0f81dd84e27c5320
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-21 22:14:37 -0700

    Create SortArrayList.java

---
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 19: Generics/SortArrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Program3 {
+    
+    public static void main(String[] args) {
+        
+    }
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+        
+    }
+}
