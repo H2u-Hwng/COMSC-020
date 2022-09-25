983ea92571ec696f8b542cfb193e63541a542b6f
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-09-24 20:45:48 -0700

    Update SortArrayList.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 19: Generics/SortArrayList.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 19: Generics/SortArrayList.java
@@ -3,22 +3,27 @@ import java.util.*;
 public class SortArrayList {
 
     public static void main(String[] args) {
-
         // Create an Integer array
-        Integer[] intArray = {new Integer(2), 
-                              new Integer(4), 
-                              new Integer(3)};
+        ArrayList<Integer> intArray = new ArrayList<>();
+        intArray.add(2);
+        intArray.add(4);
+        intArray.add(3);
         
         // Create a Double array
-        Double[] doubleArray = {new Double(3.4), 
-                                new Double(1.2),
-                                new Double(-12.3)};
-
+        ArrayList<Double> doubleArray = new ArrayList<>();
+        doubleArray.add(3.4);
+        doubleArray.add(1.2);
+        doubleArray.add(-12.3);
+        
         // Create a String array
-        String[] stringArray = {"Bob", "Alice", "Ted", "Carol"};
-
+        ArrayList<String> stringArray = new ArrayList<>();
+        stringArray.add("Bob");
+        stringArray.add("Alice");
+        stringArray.add("Ted");
+        stringArray.add("Carol");
+                  
         // Sort the arrays
-        sort(intArray);
+        Collections.sort(intArray);
 
         sort(doubleArray);
 
@@ -33,7 +38,40 @@ public class SortArrayList {
 
         System.out.print("Sorted String Objects: ");
         printList(stringArray);
+
     }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            E currentMin = list.get(i);
+            int min = i;
+            for (int j = i + 1; j < list.size(); j++) {
+
+                if (list.get(j).compareTo(currentMin) < 0) {
+                    currentMin = list.get(j);
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                list.set(min, list.get(i));
+                list.set(i, currentMin);
+            }
+        }
+    }
+    
+    /** Print an array of objects */
+    public static void printList(ArrayList list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
+
 
     public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
         E currentMin;
@@ -58,62 +96,10 @@ public class SortArrayList {
             }
         }
     }
-  /** Print an array of objects */
-
-  public static void printList(Object[] list) {
-
-    for (int i = 0; i < list.length; i++)
-
-      System.out.print(list[i] + " ");
-
-    System.out.println();
-
-  }
+  
 
 }
 
 
 
 
-    // /** Sort an array of comparable objects */
-    // public static void sort(Comparable[] list) {
-
-    // Comparable currentMax;
-
-    // int currentMaxIndex;
-
-
-
-    // for (int i = list.length - 1; i >= 1; i--) {
-
-    //   // Find the maximum in the list[0..i]
-    //   currentMax = list[i];
-
-    //   currentMaxIndex = i;
-
-
-
-    //   for (int j = i - 1; j >= 0; j--) {
-
-    //     if (currentMax.compareTo(list[j]) < 0) {
-
-    //       currentMax = list[j];
-
-    //       currentMaxIndex = j;
-
-    //     }
-
-    //   }
-
-
-
-    //   // Swap list[i] with list[currentMaxIndex] if necessary;
-    //   if (currentMaxIndex != i) {
-
-    //     list[currentMaxIndex] = list[i];
-
-    //     list[i] = currentMax;
-
-    //   }
-
-    // }
