d9cb39dce14b60d095663e5c6e343414e031f8b8
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-10-04 12:08:02 -0700

    Update SortPoints.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 20: Lists, Stacks, Queues, and Priority Queues/SortPoints.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 20: Lists, Stacks, Queues, and Priority Queues/SortPoints.java
@@ -1,34 +1,40 @@
-class Point implements Comparable<Point> {
+import java.util.*;
 
-    public double x;
-    public double y;
-  
-    public Point(int x, int y)
-		{
-			this.x=x;
-			this.y=y;
-		}
+public class SortPoints {
 
-		public int getX() {return x;}		
-		public int getY() {return y;}
-  
-    @Override
-    public int compareTo(Point o) {
-
-        if (x > o.x)
-            return 1;
-        else if (x < o.x)
-            return -1;
-        else if (y > o.y)
-            return 1;
-        else if (y < o.y)
-            return -1;
-        else
-            return 0;
+    public static void main(String[] args) {
     }
 
-    @Override
-    public String toString() {
-        return "(" + String.format("%.2f",x) + ", " + String.format("%.2f",y) + ")";
+    static class Point implements Comparable<Point> {
+    
+        double x;
+        double y;
+      
+        Point(double x, double y) {
+    	this.x = x;
+    	this.y = y;
+        }
+    
+        public double getX() {
+            return x;
+        }		
+        
+    	public double getY() {
+    	    return y;
+    	}
+      
+        @Override
+        public double compareTo(Point p) {
+    	    if (x != p.x) {
+                return x - p.x;	
+    	    } else {
+    	        return y - p.y;
+    	    }
+        }
+    
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
     }
 }
