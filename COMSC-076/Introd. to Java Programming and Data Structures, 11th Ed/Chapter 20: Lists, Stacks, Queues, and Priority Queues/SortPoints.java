e097d4283d4c41860c02d6fba85c2e72ce7468f1
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-10-07 15:16:48 -0700

    Update SortPoints.java

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 20: Lists, Stacks, Queues, and Priority Queues/SortPoints.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 20: Lists, Stacks, Queues, and Priority Queues/SortPoints.java
@@ -1,3 +1,18 @@
+/* 
+ * Huu Hung Nguyen
+ * 10/07/2022
+ * Assignment 5
+ * Define a class named Point with two data fields x and y to represent a point's x- and y-coordinates.
+ * Implement the Comparable interface for the comparing the points on x-coordinates.
+ * If two points have the same x-coordinates, compare their y-coordinates.
+ * Define a class named CompareY that implements Comparator<Point>.
+ * Implement the compare method to compare two points on their y-coordinates.
+ * If two points have the same y-coordinates, compare their x-coordinates.
+ * Randomly create 100 points and apply the Arrays.sort method to display the points
+ * in increasing order of their x-coordinates, and increasing order of their y-coordinates, respectively.
+ */
+
+
 import java.util.*;
 
 public class SortPoints {
