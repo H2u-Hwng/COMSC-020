a2c6e7649ef4c8d097b8973423c67a6b40916581
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-10-11 16:19:00 -0700

    Create TestMap.java

---
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 21: Sets and Maps/TestMap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TestMap {
+    
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        
+        // put key and value to map
+        map.put("07282000", "Huu Hung Nguyen");
+        map.put("12201969", "Thi Thuy Diem Ha");
+        map.put("05081969", "Duc Hai Nguyen");
+        
+        // display map
+        System.out.println(map);
+    }
+}
