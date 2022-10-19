8a4242ca72199e5d4f72b358255582924da44301
Huu Hung Nguyen <79944868+hhnguyen-20@users.noreply.github.com>
2022-10-18 23:07:48 -0700

    Add files via upload

--- COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 21: Sets and Maps/CountOccurrenceOfWords.java
+++ COMSC-076/Introd. to Java Programming and Data Structures, 11th Ed/Chapter 21: Sets and Maps/CountOccurrenceOfWords.java
@@ -4,52 +4,59 @@
  * Assignment 6
  * The program reads the text from a text file.
  * The text file is passed as a command-line argument.
- * Words are delimited by whitespace characters, punctuation marks (, ; . : ?),
- * quotation marks (' "), and parentheses.
- * Count the words in a case-sensitive fashion. The words must start with a letter.
- * Display the output of words in alphabetical order, with each word preceded by the number of times it occurs. 
+ * Words are delimited by whitespace characters, punctuation marks (,;.:?),
+ * quotation marks (' "), and parentheses. Count the words in a case-sensitive
+ * fashion. The words must start with a letter.
+ * Display the output of words in alphabetical order, with each word preceded
+ * by the number of times it occurs. 
  */
 
 import java.util.*;
+import java.io.*;
 
 public class CountOccurrenceOfWords {
-    
-    public static void main(String[] args) throws Exception {
-        //Check command-line argument length
-        if(args.length != 1) {
-            System.out.println("Usage: Excersice Chapter 21textFileName");
-            System.exit(1);
-        }
-            
-        //Check if file is exist
-        File file = new File(args[0]);
-        if(!file.exists()) {
-            System.out.println("The file " + args[0] + " does not exists.");
-            System.exit(1);
-        }
-        
-        // Set text in a string
-        String text = "Good morning. Have a good class. " +
-                      "Have a good visit. Have fun!";
-        
-        // Create a TreeMap to hold words as key and count as value
-        Map<String, Integer> map = new TreeMap<>();
-        
-        String[] words = text.split("[ \n\t\r\"\'.,;:!?(){}]+");
-        
-        for (String key: words) {
-            key = key.toLowerCase();
-            
-            if (map.containsKey(key)) {
-                map.put(key, map.get(key) + 1);
-            } else {
-                map.put(key, 1);
+    /** Main method */
+    public static void main(String[] args) {
+        // Create a scanner
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user for the file path
+        System.out.print("Enter the file path: ");
+        File file = new File(input.next());
+
+        try {
+            // Creating an object of BufferedReader class
+            BufferedReader bReader = new BufferedReader(new FileReader(file));
+
+            // Create a TreeMap to hold words as key and count as value
+            Map<String, Integer> map = new TreeMap<>();
+
+            String line; // Declaring a string variable
+
+            while ((line = bReader.readLine()) != null) {
+                String[] words = line.split("[0-9]+|\\W+");
+
+                for (String key: words) {
+                    key = key.toLowerCase();
+
+                    if (key.length() > 0) {
+                        if (!map.containsKey(key)) {
+                            map.put(key, 1);
+                        } else {
+                            int value = map.get(key);
+                            value++;
+                            map.put(key, value);
+                        }
+                    }
+                }
             }
+
+            // Display key and value for each entry
+            System.out.print("Display words and their count ");
+            System.out.println("in ascending order of the words");
+            map.forEach((k, v) -> System.out.println(v + "\t" + k));
+        } catch (IOException ex) {
+            System.err.println("Error in opening the file!");
         }
-        
-        // Display key and value for each entry
-        System.out.print("Display words and their count ");
-        System.out.println("in ascending order of the words");
-        map.forEach((k, v) -> System.out.println(k + "\t" + v));
     }
 }
